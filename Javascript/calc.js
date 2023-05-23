3f3a637b1520eb3308e3c6b6ed0b2aa5cec2e5e9
rakibrahat <111238255+rakibrahat@users.noreply.github.com>
2023-05-23 19:46:01 +0600

    Add files via upload

---
+++ Javascript/calc.js
@@ -0,0 +1,156 @@
+// Take Operators
+const deleteLast = document.querySelector(".remove");
+const plusSign = document.querySelector(".plus");
+const minusSign = document.querySelector(".minus");
+const multiplySign = document.querySelector(".multi");
+const divideSign = document.querySelector(".divide");
+const equalSign = document.querySelector(".equal");
+
+
+
+// Adding Event Listeners For Operators
+deleteLast.addEventListener("click", deleteTheLastChar);
+minusSign.addEventListener("click", minus)
+plusSign.addEventListener("click", add);
+multiplySign.addEventListener("click", multiply);
+divideSign.addEventListener("click", division)
+equalSign.addEventListener("click", result);
+
+
+
+// Main Calculation
+let outputText = document.getElementById("inputTexts");
+let valueTexts = outputText.textContent;
+
+function calculation(input) {
+    // let values = outputTexts;
+    let str = input;
+    let values = str.split(" ");
+    let len = values.length;
+
+    for (let i = 0; i < len; i++) {
+        if(!isNaN(values[i])) {
+            values[i] = parseFloat(values[i]);
+        }
+    }
+
+    console.log(len)
+    console.log(values)
+
+    // let total = eval(values);
+    let total = 0;
+    let operator;
+
+    for (let i = 0; i < len; i++) {
+
+        if ((values[i + 1] === "+" && i === 0) || values[i - 1] === "+") {
+            total += values[i];
+            console.log(total);
+            operator = "+";
+            console.log(operator)
+        }
+        else if ((values[i + 1] === "-" && i === 0) || values[i - 1] === "-") {
+            if ((values[i + 1] === "-" && i === 0)) {
+                total = values[i];
+                console.log(total);
+            }
+            else if (values[i - 1] === "-") {
+                total -= values[i];
+                console.log(total);
+            }
+            operator = "+";
+            console.log(operator)
+        }
+        else if ((values[i + 1] === "*" && i === 0) || values[i - 1] === "*") {
+            if ((values[i + 1] === "*" && i === 0)) {
+                total = values[i];
+            }
+            total *= values[i];
+            console.log(total);
+            operator = "*";
+            console.log(operator)
+        }
+        else if ((values[i + 1] === "/" && i === 0) || values[i - 1] === "/") {
+            if ((values[i + 1] === "/" && i === 0)) {
+                total = values[i];
+                console.log(total);
+            }
+            else if (values[i - 1] === "/") {
+                total /= values[i];
+                console.log(total);
+            }
+            console.log(total);
+            operator = "/";
+            console.log(operator)
+        }
+        else if (values[i].toString().match(/[*+-/]/ig)){
+            total = total;
+            console.log(total)
+            operator = "opp";
+            console.log(operator)
+        }
+        else if(Number(values[i])) {
+            total = values[i];
+            console.log(total)
+            operator = "numb";
+            console.log(operator)
+        }
+        else {
+            total = "Error";
+            console.log(total)
+            operator = "err";
+            console.log(operator)
+        }
+    }
+
+    console.log(total);
+    resultTexts.innerHTML = total;
+    return total;
+}
+
+// Addition
+function add() {
+    outputTexts.innerHTML += " + ";
+}
+
+// Substraction
+function minus() {
+    outputTexts.innerHTML += " - ";
+
+}
+
+
+// Multiplication
+function multiply() {
+    outputTexts.innerHTML += " * ";
+}
+
+// Division
+function division() {
+    outputTexts.innerHTML += " / ";
+}
+
+// Final Result
+function result() {
+
+    console.log(outputText.textContent)
+
+    calculation(outputText.textContent);
+
+    lastResult = calculation(outputText.textContent);
+    outputTexts.innerHTML = lastResult;
+    resultTexts.innerHTML = "Ans";
+    console.log(lastResult)
+}
+
+
+
+// Delete Last Number
+function deleteTheLastChar() {
+    if (outputTexts.textContent[outputTexts.textContent.length - 1] === " ") {
+        outputTexts.innerHTML = outputTexts.textContent.slice(0, -3);
+    }
+    else {
+        outputTexts.innerHTML = outputTexts.textContent.slice(0, -1);
+    }
+}
---
+++ Javascript/script.js
@@ -0,0 +1,76 @@
+// Make var for result
+const refreshBtn = document.querySelector(".refresh-btn");
+refreshBtn.addEventListener("click", refresh);
+
+let resultTexts = document.getElementById("resultTexts")
+resultTexts.innerHTML = "";
+
+let inputValue;
+let inputNumber;
+let lastResult = 0;
+let lastAnswer;
+let initialResult;
+
+
+// Output Element
+let outputTexts = document.getElementById("inputTexts");
+
+// Take The ans button
+const ans = document.querySelector(".ans");
+
+
+// Adding Event Listeners For Numbers
+ans.addEventListener("click", answerAdd);
+
+// Function for number input
+
+function clickOnNumber(num) {
+
+    outputReset(outputTexts);
+
+    outputTexts.innerHTML += `${num}`;
+
+    initialResult = calculation(outputTexts.textContent);
+
+    primaryResult(initialResult);
+
+    console.log(outputTexts.textContent)
+}
+
+function answerAdd() {
+
+    lastAnswer = lastResult;
+
+    outputReset(outputTexts);
+
+    inputValue = `${lastResult}`;
+
+    inputNumber = Number(inputValue);
+
+    outputTexts.innerHTML += `${inputNumber}`;
+    resultTexts.innerHTML = "Ans";
+
+    lastResult = 0;
+
+    console.log(outputTexts.textContent);
+}
+
+
+function primaryResult(initialResult) {
+    resultTexts.innerHTML = initialResult;
+}
+
+
+// Reset Functions
+function refresh() {
+    lastAnswer = 0;
+    lastResult = 0;
+    outputTexts.innerHTML = "0";
+    resultTexts.innerHTML = "";
+}
+
+function outputReset(htmlContent) {
+    if(htmlContent.textContent === "0" || htmlContent.textContent === `${lastResult}`){
+        htmlContent.innerHTML = "";
+    }
+}
